ee589c7e4085e6b2089e9ec9e05f4411d0ae6325
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-09-17 22:22:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+﻿#include "thread_pool"
+#include <iostream>
+#include "quick_sort.h"
+
+std::shared_ptr<int[]> array(new int[arrSize]);
+std::vector<std::thread> threads;
+std::mutex mtx;
+QuickSortPool(array3, 0, arrSize - 1);
