9e8b1a636bf9d8e9a9e5b409017a519faf9f8a22
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-09-17 21:51:12 +0300

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include "request_handler.h"
+
+long constexpr arrSize = 1000000;
+size_t constexpr threadCount = 10;
+RequestHandler rh;
+
+void QuickSortPool(std::shared_ptr<int[]> array, long left, long right)
+{
+  if (left >= right)
+    return;
+  long leftBound = left;
+  long rightBound = right;
+  long middle = array[(static_cast<ptrdiff_t>(leftBound) + rightBound) / 2];
+  do
+  {
+    while (array[leftBound] < middle)
+      ++leftBound;
+    while (array[rightBound] > middle)
+      --rightBound;
+    if (leftBound <= rightBound)
+    {
+      std::swap(array[leftBound], array[rightBound]);
+      ++leftBound;
+      --rightBound;
+    }
+  } while (leftBound <= rightBound);
+
+  if (rightBound - left > 100000)
+  {
+    std::packaged_task<void()>task([=] ()
+      { rh.PushRequest(QuickSortPool, array, left, rightBound); });
+    QuickSortPool(array, leftBound, right);
+    std::future<void> fut = task.get_future();
+    task();
+  }
+  else
+  {
+    QuickSortPool(array, left, rightBound);
+    QuickSortPool(array, leftBound, right);
+  }
+}
