62a7fa481f2008b145fb4b4a6356538d8180f74c
ArtemSychev-cpu <125487729+ArtemSychev-cpu@users.noreply.github.com>
2023-09-17 22:24:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include <iostream>
 #include "quick_sort.h"
 
+int main(){
+
 std::shared_ptr<int[]> array(new int[arrSize]);
 std::vector<std::thread> threads;
 std::mutex mtx;
 QuickSortPool(array3, 0, arrSize - 1);
+
+return 0;
+}
